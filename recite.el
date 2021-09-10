30e42c08f9829f049f479b6c085fa76ba793ed35
xcyzxcyz <xcyzxcyz3@126.com>
2021-09-10 15:28:26 +0800

    ggg

--- recite.el
+++ recite.el
@@ -1,8 +1,3 @@
-(defvar my-shell-directory
-  (if load-file-name
-      (file-name-directory load-file-name)
-    default-directory))
-
 ;;;###autoload
 (defun recite-mode ()
   (interactive)
@@ -11,6 +6,8 @@
          (choices
           '("word" "sentence" "sentence2" "random"))
          (mychoice (ido-completing-read          "mode:" choices))
+         (my-shell-directory
+          (if load-file-name              (file-name-directory load-file-name)            default-directory))
          (cmd (format "python %srecite.py '%s' %s"
                       dir
                       text
